13b7f093cf392bbd28c2cf11cb2c5dafce8985f4
Federico Prati <FEDERICO.PRATI@local.ittcesena.it>
2024-04-30 12:39:22 +0200

    risoluzioni problemi tecnici

--- SolitarioFrankie/obj/Debug/net7.0-windows/SolitarioFrankie.AssemblyInfo.cs
+++ SolitarioFrankie/obj/Debug/net7.0-windows/SolitarioFrankie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolitarioFrankie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61d09406e7add1d50788966a05ee68909d76e74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolitarioFrankie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolitarioFrankie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
